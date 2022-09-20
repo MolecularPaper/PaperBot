22e323d283e8e01743baadca17e9d361e809aab7
MolecularPaper <jat05026@gmail.com>
2022-09-20 10:45:42 +0900

    Update
    [Added]
    1. Paper Bot Info

---
+++ Source/Moudles/paperInfo.mjs
@@ -0,0 +1,16 @@
+import { MoudleBase } from "./moudleBase.mjs";
+import { EmbedBuilder } from "discord.js";
+import { readFileSync } from 'fs'
+
+export class PaperInfo extends MoudleBase{
+    constructor(commandName){
+        super(commandName);
+        this.infoEmbed = new EmbedBuilder().setColor(0x00ff00).setTitle("페이퍼 정보")
+        this.info = readFileSync("./Data/paperInfo.txt", "utf-8");
+        this.infoEmbed.setDescription(this.info)
+    }
+
+    async entry(interaction){
+        await interaction.reply({embeds: [this.infoEmbed]});
+    }
+}
--- Source/System/moudleGenerator.mjs
+++ Source/System/moudleGenerator.mjs
@@ -1,5 +1,7 @@
 import { Meal } from '../Moudles/todaysMeal.mjs';
+import { PaperInfo } from '../Moudles/paperInfo.mjs';
 
 export var moudels = [
-    new Meal("식사추천")
+    new Meal("식사추천"),
+    new PaperInfo("정보")
 ];
