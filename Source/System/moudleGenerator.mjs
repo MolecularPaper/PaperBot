12a7f1782ab2116b94a753bc958f6ad263d0daa3
MolecularPaper <jat05026@gmail.com>
2022-09-20 16:05:14 +0900

    Update

--- Source/System/moudleGenerator.mjs
+++ Source/System/moudleGenerator.mjs
@@ -1,9 +1,7 @@
 import { Meal } from '../Moudles/todaysMeal.mjs';
 import { PaperInfo } from '../Moudles/paperInfo.mjs';
-import { Chatting } from '../Moudles/chatting.mjs';
 
 export var moudels = [
-    new Chatting("대화생성"),
     new Meal("식사추천"),
     new PaperInfo("정보")
 ];
